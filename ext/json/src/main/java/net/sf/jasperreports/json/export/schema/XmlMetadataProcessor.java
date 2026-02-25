2ca12e21be07fcc6366b55ea9f2d56178d9551ec
Narcis Marcu <narcism@users.sourceforge.net>
2026-02-25 18:42:30 +0200

    Added the XmlMetadataExporter

---
+++ ext/json/src/main/java/net/sf/jasperreports/json/export/schema/XmlMetadataProcessor.java
@@ -0,0 +1,46 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2025 Cloud Software Group, Inc. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.json.export.schema;
+
+import java.io.Writer;
+
+
+/**
+ * @author Narcis Marcu (narcism@users.sourceforge.net)
+ */
+public class XmlMetadataProcessor extends AbstractMetadataProcessor {
+
+	public XmlMetadataProcessor(Writer writer) {
+		super(new JsonSchema(), new XmlMetadataWriter(writer));
+	}
+
+	public XmlMetadataProcessor(JsonSchema jsonSchema, Writer writer) {
+		super(jsonSchema, new XmlMetadataWriter(writer));
+	}
+
+	@Override
+	public XmlMetadataWriter getMetadataWriter() {
+		return (XmlMetadataWriter)metadataWriter;
+	}
+}
