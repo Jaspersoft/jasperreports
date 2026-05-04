Jaspersoft/jasperreports

commit 6639bfca2816e5f4f92b8d89f54155090d383b0c

add legacy property for pdf page anchors

core/src/main/resources/default.jasperreports.properties
@@ -90,6 +90,8 @@ net.sf.jasperreports.export.pdf.producer.factory=net.sf.jasperreports.pdf.classi
 net.sf.jasperreports.export.pdf.embed.icc.profile=false
 net.sf.jasperreports.export.pdf.use.cmyk.colors=false
 net.sf.jasperreports.export.pdf.legacy.text.measuring.fix=false
+net.sf.jasperreports.export.pdf.legacy.target.blank.links=false
+net.sf.jasperreports.export.pdf.legacy.page.anchors=false
 
 # XLS exporter settings
 net.sf.jasperreports.export.xls.create.custom.palette=false

ext/pdf/config.reference.xml
@@ -382,8 +382,21 @@ This should be enabled only when the legacy text measuring is enabled with the <
 otherwise the additional pixel in PDF text rendering is not needed since the newer text measuring code no longer truncates text height.
     </description>
   </configProperty>
-
-
+  
+  
+  <!-- 	net.sf.jasperreports.export.pdf.legacy.page.anchors -->
+  
+  <configProperty name="net.sf.jasperreports.export.pdf.legacy.page.anchors">
+    <description>
+Flag that enables legacy page anchors in PDF.
+<br/>
+Before version 7.0.7, the PDF exporter created page anchors in PDF by adding an empty text chunk using default font.
+Starting from version 7.0.7, these page anchors are no longer created by default, but the property can be set to true to 
+reinstate the behaviour.
+    </description>
+  </configProperty>
+  
+  
   <!-- net.sf.jasperreports.export.pdf.metadata.author -->
   
   <configProperty name="net.sf.jasperreports.export.pdf.metadata.author">

ext/pdf/src/main/java/net/sf/jasperreports/pdf/JRPdfExporter.java
@@ -535,6 +535,15 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 		)
 	public static final String LEGACY_TEXT_MEASURING_FIX = PDF_EXPORTER_PROPERTIES_PREFIX + "legacy.text.measuring.fix";
 	
+	@Property(
+		category = PropertyConstants.CATEGORY_EXPORT,
+		scopes = {PropertyScope.GLOBAL, PropertyScope.CONTEXT, PropertyScope.REPORT},
+		sinceVersion = PropertyConstants.VERSION_7_0_7,
+		valueType = Boolean.class,
+		defaultValue = PropertyConstants.BOOLEAN_FALSE
+		)
+	public static final String LEGACY_PAGE_ANCHORS = PDF_EXPORTER_PROPERTIES_PREFIX + "legacy.page.anchors";
+	
 	/**
 	 * The exporter key, as used in
 	 * {@link GenericElementHandlerEnviroment#getElementHandler(JRGenericElementType, String)}.
@@ -1082,6 +1091,8 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 
 				setPageSize(null);
 				
+				boolean legacyPageAnchor = propertiesUtil.getBooleanProperty(jasperPrint, LEGACY_PAGE_ANCHORS, false);
+				
 				boolean pageExported = false;
 				List<JRPrintPage> pages = jasperPrint.getPages();
 				if (pages != null && pages.size() > 0)
@@ -1127,7 +1138,10 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 
 						pdfProducer.getPdfContent().setLineCap(LineCapStyle.PROJECTING_SQUARE);
 
-						writePageAnchor(pageIndex);
+						if (legacyPageAnchor)
+						{
+							writePageAnchor(pageIndex);
+						}
 						
 						crtDocumentPageNumber++;
 

core/src/main/java/net/sf/jasperreports/properties/PropertyConstants.java
@@ -277,5 +277,7 @@ public interface PropertyConstants
 	String VERSION_7_0_4 = "7.0.4";
 	
 	String VERSION_7_0_6 = "7.0.6";
+	
+	String VERSION_7_0_7 = "7.0.7";
 
 }

commit c93faea49e2911b66ccda0372c958bacdf336da0

fix local page links in PDF
regression since 6639bfca2816e5f4f92b8d89f54155090d383b0c

also completely remove page anchors (with legacy property)

core/src/main/resources/default.jasperreports.properties
@@ -91,7 +91,6 @@ net.sf.jasperreports.export.pdf.embed.icc.profile=false
 net.sf.jasperreports.export.pdf.use.cmyk.colors=false
 net.sf.jasperreports.export.pdf.legacy.text.measuring.fix=false
 net.sf.jasperreports.export.pdf.legacy.target.blank.links=false
-net.sf.jasperreports.export.pdf.legacy.page.anchors=false
 
 # XLS exporter settings
 net.sf.jasperreports.export.xls.create.custom.palette=false

ext/pdf/config.reference.xml
@@ -382,21 +382,8 @@ This should be enabled only when the legacy text measuring is enabled with the <
 otherwise the additional pixel in PDF text rendering is not needed since the newer text measuring code no longer truncates text height.
     </description>
   </configProperty>
-  
-  
-  <!-- 	net.sf.jasperreports.export.pdf.legacy.page.anchors -->
-  
-  <configProperty name="net.sf.jasperreports.export.pdf.legacy.page.anchors">
-    <description>
-Flag that enables legacy page anchors in PDF.
-<br/>
-Before version 7.0.7, the PDF exporter created page anchors in PDF by adding an empty text chunk using default font.
-Starting from version 7.0.7, these page anchors are no longer created by default, but the property can be set to true to 
-reinstate the behaviour.
-    </description>
-  </configProperty>
-  
-  
+
+
   <!-- net.sf.jasperreports.export.pdf.metadata.author -->
   
   <configProperty name="net.sf.jasperreports.export.pdf.metadata.author">

ext/pdf/src/main/java/net/sf/jasperreports/pdf/JRPdfExporter.java
@@ -535,15 +535,6 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 		)
 	public static final String LEGACY_TEXT_MEASURING_FIX = PDF_EXPORTER_PROPERTIES_PREFIX + "legacy.text.measuring.fix";
 	
-	@Property(
-		category = PropertyConstants.CATEGORY_EXPORT,
-		scopes = {PropertyScope.GLOBAL, PropertyScope.CONTEXT, PropertyScope.REPORT},
-		sinceVersion = PropertyConstants.VERSION_7_0_7,
-		valueType = Boolean.class,
-		defaultValue = PropertyConstants.BOOLEAN_FALSE
-		)
-	public static final String LEGACY_PAGE_ANCHORS = PDF_EXPORTER_PROPERTIES_PREFIX + "legacy.page.anchors";
-	
 	/**
 	 * The exporter key, as used in
 	 * {@link GenericElementHandlerEnviroment#getElementHandler(JRGenericElementType, String)}.
@@ -553,11 +544,6 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 	public static final String PDF_PRODUCER_FACTORY_PROPERTY = PDF_EXPORTER_PROPERTIES_PREFIX + "producer.factory";
 	
 	private static final String EMPTY_BOOKMARK_TITLE = "";
-
-	/**
-	 *
-	 */
-	protected static final String JR_PAGE_ANCHOR_PREFIX = "JR_PAGE_ANCHOR_";
 
 	protected static boolean fontsRegistered;
 	
@@ -596,6 +582,8 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 	protected int reportIndex;
 	protected PrintPageFormat pageFormat;
 	protected int crtDocumentPageNumber;
+	protected int crtReportStartPageIndex;
+	protected int crtReportPdfPageStart;
 	
 	protected int permissions;
 
@@ -1091,8 +1079,6 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 
 				setPageSize(null);
 				
-				boolean legacyPageAnchor = propertiesUtil.getBooleanProperty(jasperPrint, LEGACY_PAGE_ANCHORS, false);
-				
 				boolean pageExported = false;
 				List<JRPrintPage> pages = jasperPrint.getPages();
 				if (pages != null && pages.size() > 0)
@@ -1118,6 +1104,9 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 					int startPageIndex = (pageRange == null || pageRange.getStartPageIndex() == null) ? 0 : pageRange.getStartPageIndex();
 					int endPageIndex = (pageRange == null || pageRange.getEndPageIndex() == null) ? (pages.size() - 1) : pageRange.getEndPageIndex();
 
+					crtReportStartPageIndex = startPageIndex;
+					crtReportPdfPageStart = crtDocumentPageNumber + 1;
+
 					for (int pageIndex = startPageIndex; pageIndex <= endPageIndex; pageIndex++)
 					{
 						checkInterrupted();
@@ -1138,11 +1127,6 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 
 						pdfProducer.getPdfContent().setLineCap(LineCapStyle.PROJECTING_SQUARE);
 
-						if (legacyPageAnchor)
-						{
-							writePageAnchor(pageIndex);
-						}
-						
 						crtDocumentPageNumber++;
 
 						/*   */
@@ -1189,32 +1173,6 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 		//return os.toByteArray();
 	}
 
-
-	protected void writePageAnchor(int pageIndex) 
-	{
-		Map<Attribute,Object> attributes = new HashMap<>();
-		fontUtil.getAttributesWithoutAwtFont(attributes, new JRBasePrintText(jasperPrint.getDefaultStyleProvider()));
-		PdfTextChunk chunk = pdfProducer.createChunk(" ", attributes, getLocale());
-		
-		chunk.setLocalDestination(JR_PAGE_ANCHOR_PREFIX + reportIndex + "_" + (pageIndex + 1));
-
-		tagHelper.beginArtifact();
-		
-		PdfPhrase phrase = pdfProducer.createPhrase(chunk);
-		
-		phrase.go(
-			0,
-			pageFormat.getPageHeight(),
-			1,
-			1,
-			0,
-			0,
-			PdfTextAlignment.LEFT,
-			TextDirection.DEFAULT
-			);
-
-		tagHelper.endArtifact();
-	}
 
 	/**
 	 *
@@ -2250,7 +2208,10 @@ public class JRPdfExporter extends JRAbstractExporter<PdfReportConfiguration, Pd
 					{
 						if (link.getHyperlinkPage() != null)
 						{
-							chunk.setLocalGoto(JR_PAGE_ANCHOR_PREFIX + reportIndex + "_" + link.getHyperlinkPage().toString());
+							int pdfPage = crtReportPdfPageStart + (link.getHyperlinkPage() - 1 - crtReportStartPageIndex);
+							int targetPageIndex = link.getHyperlinkPage() - 1;
+							float targetPageHeight = jasperPrint.getPageFormat(targetPageIndex).getPageHeight();
+							chunk.setLocalGotoPage(pdfPage, targetPageHeight);
 							wasHyperlinkSet = true;
 						}
 						break;

ext/pdf/src/main/java/net/sf/jasperreports/pdf/classic/ClassicChunk.java
@@ -29,6 +29,7 @@ import com.lowagie.text.pdf.PdfAction;
 import com.lowagie.text.pdf.PdfAnnotation;
 import com.lowagie.text.pdf.PdfArray;
 import com.lowagie.text.pdf.PdfBorderArray;
+import com.lowagie.text.pdf.PdfDestination;
 import com.lowagie.text.pdf.PdfDictionary;
 import com.lowagie.text.pdf.PdfName;
 import com.lowagie.text.pdf.PdfNumber;
@@ -139,6 +140,28 @@ public class ClassicChunk implements PdfChunk
 		else
 		{
 			chunk.setLocalGoto(anchor);
+		}
+	}
+
+	@Override
+	public void setLocalGotoPage(int page, float top)
+	{
+		PdfAction action = PdfAction.gotoLocalPage(page, new PdfDestination(PdfDestination.XYZ, 0, top, 0), pdfProducer.getPdfWriter());
+		if (linkTag != null)
+		{
+			addAnnotationToTag(
+				linkTag,
+				PdfAnnotation.createLink(
+					pdfProducer.getPdfWriter(),
+					new Rectangle(linkLlx, linkLly, linkUrx, linkUry),
+					PdfAnnotation.HIGHLIGHT_INVERT,
+					action
+					)
+				);
+		}
+		else
+		{
+			chunk.setAction(action);
 		}
 	}
 

ext/pdf/src/main/java/net/sf/jasperreports/pdf/common/PdfChunk.java
@@ -38,6 +38,8 @@ public interface PdfChunk
 
 	void setLocalGoto(String anchor);
 
+	void setLocalGotoPage(int page, float top);
+
 	void setRemoteGoto(String reference, String anchor);
 
 	void setRemoteGoto(String reference, int page);