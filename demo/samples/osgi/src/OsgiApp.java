988cc286057421090969aa5bc8a2275d89ee8b13
Teodor Danciu <teodord@users.sourceforge.net>
2026-05-05 21:05:31 +0300

    add osgi sample

---
+++ demo/samples/osgi/src/OsgiApp.java
@@ -0,0 +1,276 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2025 Cloud Software Group, Inc. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.felix.framework.FrameworkFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.launch.Framework;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.util.AbstractSampleApp;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ */
+public class OsgiApp extends AbstractSampleApp
+{
+	private Framework framework;
+	private Bundle jrBundle;
+
+
+	/**
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		main(new OsgiApp(), args);
+	}
+
+
+	@Override
+	public void test() throws JRException
+	{
+		startFramework();
+		try
+		{
+			installBundles();
+			fill();
+			//xml();
+		}
+		finally
+		{
+			stopFramework();
+		}
+	}
+
+
+	public void startFramework() throws JRException
+	{
+		try
+		{
+			Map<String, String> config = new HashMap<String, String>();
+			config.put("org.osgi.framework.storage", "target/felix-cache");
+			config.put("org.osgi.framework.storage.clean", "onFirstInit");
+			config.put("felix.log.level", "1");
+			config.put("org.osgi.framework.bundle.parent", "framework");
+			config.put(
+				"org.osgi.framework.bootdelegation",
+				"sun.*,com.sun.*,"
+				+ "javax.*,"
+				+ "org.xml.*,org.w3c.*,"
+				+ "com.fasterxml.*,"
+				+ "org.apache.*,"
+				+ "org.eclipse.*"
+			);
+
+			FrameworkFactory frameworkFactory = new FrameworkFactory();
+			framework = frameworkFactory.newFramework(config);
+			framework.start();
+
+			System.out.println("OSGi Framework [" + framework.getSymbolicName() + " " + framework.getVersion() + "] started.");
+		}
+		catch (Exception e)
+		{
+			throw new JRException("Failed to start OSGi framework", e);
+		}
+	}
+
+
+	public void installBundles() throws JRException
+	{
+		File[] files = getFiles(new File("target/dependency"), "jar");
+		for (File jarFile : files)
+		{
+			try
+			{
+				if (jarFile.getName().startsWith("jasperreports-"))
+				{
+					jarFile = OsgiUtil.alterOsgiBundle(jarFile);
+				}
+
+				Bundle bundle = installBundle(jarFile);
+
+				if ("net.sf.jasperreports.core".equals(bundle.getSymbolicName()))
+				{
+					jrBundle = bundle;
+				}
+			}
+			catch (Exception e)
+			{
+				throw new JRException("Failed to install bundle: " + jarFile.getAbsolutePath(), e);
+			}
+		}
+
+		if (jrBundle != null)
+		{
+			try
+			{
+				jrBundle.start();
+			}
+			catch (Exception e)
+			{
+				throw new JRException("Failed to start core bundle", e);
+			}
+		}
+
+		BundleContext bundleContext = framework.getBundleContext();
+		Bundle[] bundles = bundleContext.getBundles();
+		System.out.println("Installed bundles:");
+		for (Bundle bundle : bundles)
+		{
+			System.out.println(
+				"  " + bundle.getBundleId()
+				+ " - " + bundle.getSymbolicName()
+				+ " " + bundle.getVersion()
+				+ " [" + OsgiUtil.getBundleState(bundle) + "]"
+			);
+		}
+	}
+
+
+	private Bundle installBundle(File bundleJar) throws Exception
+	{
+		BundleContext bundleContext = framework.getBundleContext();
+		String bundleLocation = "file:" + bundleJar.getAbsolutePath();
+		System.out.println("Installing: " + bundleLocation);
+		Bundle bundle = bundleContext.installBundle(bundleLocation);
+		System.out.println("Installed bundle: " + bundle.getSymbolicName() + " " + bundle.getVersion());
+		return bundle;
+	}
+
+
+	public void fill() throws JRException
+	{
+		long start = System.currentTimeMillis();
+
+		ClassLoader originalCL = Thread.currentThread().getContextClassLoader();
+		try
+		{
+			Class<?> fillManagerClass = jrBundle.loadClass("net.sf.jasperreports.engine.JasperFillManager");
+			ClassLoader bundleCL = fillManagerClass.getClassLoader();
+			Thread.currentThread().setContextClassLoader(bundleCL);
+
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put("ReportTitle", "OSGi Embedded Framework Report");
+			parameters.put("FrameworkInfo", framework.getSymbolicName() + " " + framework.getVersion());
+
+			StringBuilder bundleList = new StringBuilder();
+			BundleContext bundleContext = framework.getBundleContext();
+			for (Bundle bundle : bundleContext.getBundles())
+			{
+				if (bundleList.length() > 0)
+				{
+					bundleList.append("\n");
+				}
+				bundleList.append(bundle.getSymbolicName())
+					.append(" ").append(bundle.getVersion())
+					.append(" [").append(OsgiUtil.getBundleState(bundle)).append("]");
+			}
+			parameters.put("BundleList", bundleList.toString());
+
+			Class<?> loadedClass = jrBundle.loadClass("net.sf.jasperreports.engine.JasperFillManager");
+			parameters.put(
+				"ClassLoaderInfo",
+				"JasperFillManager classloader: " + loadedClass.getClassLoader()
+				+ "\nApplication classloader: " + getClass().getClassLoader()
+				+ "\nClasses loaded from different classloaders: " + (loadedClass.getClassLoader() != getClass().getClassLoader())
+			);
+
+			System.out.println("Filling report via bundle-loaded JasperFillManager...");
+
+			Method fillMethod = fillManagerClass.getMethod(
+				"fillReportToFile", String.class, String.class, Map.class
+			);
+			fillMethod.invoke(
+				null,
+				"target/reports/OsgiReport.jasper",
+				"target/reports/OsgiReport.jrprint",
+				parameters
+			);
+		}
+		catch (Exception e)
+		{
+			throw new JRException("Failed to fill report via OSGi bundle", e);
+		}
+		finally
+		{
+			Thread.currentThread().setContextClassLoader(originalCL);
+		}
+
+		System.out.println("Filling time : " + (System.currentTimeMillis() - start));
+	}
+
+
+	public void xml() throws JRException
+	{
+		long start = System.currentTimeMillis();
+
+		ClassLoader originalCL = Thread.currentThread().getContextClassLoader();
+		try
+		{
+			Class<?> exportManagerClass = jrBundle.loadClass("net.sf.jasperreports.engine.JasperExportManager");
+			Thread.currentThread().setContextClassLoader(exportManagerClass.getClassLoader());
+
+			System.out.println("Exporting report via bundle-loaded JasperExportManager...");
+
+			Method exportMethod = exportManagerClass.getMethod(
+				"exportReportToXmlFile", String.class, boolean.class
+			);
+			exportMethod.invoke(null, "target/reports/OsgiReport.jrprint", false);
+		}
+		catch (Exception e)
+		{
+			throw new JRException("Failed to export report via OSGi bundle", e);
+		}
+		finally
+		{
+			Thread.currentThread().setContextClassLoader(originalCL);
+		}
+
+		System.out.println("XML export time : " + (System.currentTimeMillis() - start));
+	}
+
+
+	public void stopFramework() throws JRException
+	{
+		if (framework != null)
+		{
+			try
+			{
+				framework.stop();
+				framework.waitForStop(5000);
+				System.out.println("OSGi framework stopped.");
+			}
+			catch (Exception e)
+			{
+				throw new JRException("Failed to stop OSGi framework", e);
+			}
+		}
+	}
+}
---
+++ demo/samples/osgi/src/OsgiUtil.java
@@ -0,0 +1,164 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2025 Cloud Software Group, Inc. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+
+import org.osgi.framework.Bundle;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ */
+public class OsgiUtil
+{
+	/**
+	 * Copies the JasperRerports JAR with a corrected Bundle-Version in the manifest,
+	 * because development version strings (e.g. "develop-JS-78982-SNAPSHOT") are not valid OSGi versions.
+	 */
+	public static File alterOsgiBundle(File sourceJar) throws Exception
+	{
+		JarFile jarFile = new JarFile(sourceJar);
+		try
+		{
+			Manifest manifest = jarFile.getManifest();
+			String version = manifest.getMainAttributes().getValue("Bundle-Version");
+			if (version != null)
+			{
+				manifest.getMainAttributes().putValue("Bundle-Version", toOsgiVersion(version));
+			}
+
+			File bundleJar = new File("target/osgi-bundle-cache/" + sourceJar.getName());
+			bundleJar.getParentFile().mkdirs();
+
+			JarOutputStream jos = new JarOutputStream(new FileOutputStream(bundleJar), manifest);
+			try
+			{
+				byte[] buffer = new byte[8192];
+				Enumeration<JarEntry> entries = jarFile.entries();
+				while (entries.hasMoreElements())
+				{
+					JarEntry entry = entries.nextElement();
+					if ("META-INF/MANIFEST.MF".equals(entry.getName()))
+					{
+						continue;
+					}
+					jos.putNextEntry(new JarEntry(entry.getName()));
+					if (!entry.isDirectory())
+					{
+						InputStream is = jarFile.getInputStream(entry);
+						try
+						{
+							int read;
+							while ((read = is.read(buffer)) != -1)
+							{
+								jos.write(buffer, 0, read);
+							}
+						}
+						finally
+						{
+							is.close();
+						}
+					}
+					jos.closeEntry();
+				}
+			}
+			finally
+			{
+				jos.close();
+			}
+
+			return bundleJar;
+		}
+		finally
+		{
+			jarFile.close();
+		}
+	}
+
+
+	private static String toOsgiVersion(String mavenVersion)
+	{
+		// OSGi versions: major.minor.micro.qualifier (integers + alphanumeric qualifier with no hyphens)
+		String cleaned = mavenVersion.replace("-", ".");
+		String[] parts = cleaned.split("\\.");
+		StringBuilder osgiVersion = new StringBuilder();
+		int numericCount = 0;
+		StringBuilder qualifier = new StringBuilder();
+		for (String part : parts)
+		{
+			if (numericCount < 3 && part.matches("\\d+"))
+			{
+				if (numericCount > 0)
+				{
+					osgiVersion.append(".");
+				}
+				osgiVersion.append(part);
+				numericCount++;
+			}
+			else
+			{
+				if (qualifier.length() > 0)
+				{
+					qualifier.append("_");
+				}
+				qualifier.append(part);
+			}
+		}
+		while (numericCount < 3)
+		{
+			if (numericCount > 0)
+			{
+				osgiVersion.append(".");
+			}
+			osgiVersion.append("0");
+			numericCount++;
+		}
+		if (qualifier.length() > 0)
+		{
+			osgiVersion.append(".").append(qualifier);
+		}
+		return osgiVersion.toString();
+	}
+
+
+	public static String getBundleState(Bundle bundle)
+	{
+		switch (bundle.getState())
+		{
+			case Bundle.UNINSTALLED : return "UNINSTALLED";
+			case Bundle.INSTALLED : return "INSTALLED";
+			case Bundle.RESOLVED : return "RESOLVED";
+			case Bundle.STARTING : return "STARTING";
+			case Bundle.STOPPING : return "STOPPING";
+			case Bundle.ACTIVE : return "ACTIVE";
+			default : return "UNKNOWN";
+		}
+	}
+}
